87d2a645c81fd4695294f559891a4f7c31821ed6
Janice Jim <janice@tecky.io>
2024-02-12 19:51:53 +0800

    edit db.ts to set the knex env

--- utils/db.ts
+++ utils/db.ts
@@ -1,15 +1,6 @@
-import { Client } from "pg";
 import { env } from "./env";
-
-export const client = new Client({
-  user: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
-  host: env.DB_HOST,
-});
-client.connect();
 
 import Knex from "knex";
 const knexConfig = require("../knexfile");
 
-export let knex = Knex(knexConfig["development"]);
+export let knex = Knex(knexConfig[env.NODE_ENV || "development"]);
